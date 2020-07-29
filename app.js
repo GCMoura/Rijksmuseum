fb4067961d93f0baa32dc929850b745196c2e709
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-07-28 22:58:20 -0300

    falta try/catch

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ var colors = [
     '#e37222', //laranja
     '#0e0b16', //preto
     'pink', //rosa
-    '#015249', //verde
+    '#0375b4', //azul
     '#3cc47c' //verde
 ]
 var input = document.querySelector('.input')
 var btnSearch = document.querySelector('.btn')
 var cleaner = document.querySelector('.btn-limpar')
-//var btnMakersList = document.querySelector('.makers-list button')
 var btnMaker = document.querySelector('.button-maker')
 var content = document.querySelector('.content') 
 
@@ -29,66 +28,13 @@ var page = 1
 
 var search = '' 
 
-var title = []
-var maker = []
-
 btnSearch.addEventListener('click', getSetByMakerAndArt) //botão de pesquisa
 
 cleaner.addEventListener('click', cleanField) //botão para limpar a pesquisa
 
-//btnMakersList.addEventListener('click', makersList) //botão com a lista de pintores
-
-//getMuseum()
 changeColor()
 
-// async function getMuseum() { //faz a consulta à API do museu
-    
-//     //const url = `https://www.rijksmuseum.nl/api/en/collection?key=${key}&involvedMaker=${search}&ps=100` //pesquisa por nome do pintor
-//     //const url = `https://www.rijksmuseum.nl/api/en/collection?key=${key}&q=${search}&ps=100`  //pesquisa por termo de busca
-
-//     const url = `https://www.rijksmuseum.nl/api/en/collection?key=${key}&q=${search}&ps=100&p=${page}`
-
-//     const res = await fetch(url)
-   
-//     masterpieces = await res.json()
-
-//     btnSearch.disabled = false
-//     btnMakersList.disabled = false
-    
-//     console.log(masterpieces)
-//     console.log('total de obras: ', masterpieces.count)
-//     console.log('pintor: ', masterpieces.artObjects[2].principalOrFirstMaker)
-//     console.log('titulo: ', masterpieces.artObjects[2].title)
-//     console.log('imagem: ', masterpieces.artObjects[2].webImage.url)
-//     console.log('imagem: ', masterpieces.artObjects[2])
-
-
-
-//     if(masterpieces.artObjects.length === 0){ //o retorno do fetch foi vazio
-//         location.reload() //reload a página
-//     }
-// }
-
-// function makersList(){ //cria a lista de botões com todos os pintores
-
-//     var makersArray = masterpieces.facets[0].facets
-
-//     for(let i = 0; i < makersArray.length; i++){
-//         var button = document.createElement('button')
-//         button.classList.add('btn-maker')
-//         if(makersArray[i].key !== 'anonymous' && makersArray[i].key !== 'unknown') {
-//             button.innerHTML = makersArray[i].key
-//             btnMaker.appendChild(button)
-//         }
-//     }
-//     var buttons = document.querySelectorAll('.btn-maker')
-
-//     buttons.forEach(button => {
-//         button.addEventListener('click', function(){getSets(button.firstChild.data)})
-//     }) 
-// }
-
-async function getSetByMakerAndArt(){
+async function getSetByMakerAndArt(){ //acessa a API e retorna as obras relacionadas ao termo pesquisado
 
     search = input.value //pega o valor da pesquisa
 
@@ -98,16 +44,13 @@ async function getSetByMakerAndArt(){
    
     masterpieces = await res.json()
     
-    console.log(masterpieces)
-
-    var count = 1
+    //console.log(masterpieces)
 
     for(let i = 0; i < masterpieces.artObjects.length; i++){
         var id = masterpieces.artObjects[i].id.split('-')
         if(id[1] === 'SK' ){
             if(masterpieces.artObjects[i].principalOrFirstMaker !== "anonymous"){
                 createSetButton(masterpieces.artObjects[i].title, masterpieces.artObjects[i].principalOrFirstMaker, masterpieces.artObjects[i].objectNumber)
-                count++
             }
         }
     }
@@ -115,11 +58,15 @@ async function getSetByMakerAndArt(){
     page++
     if(page <= Math.floor(masterpieces.count / 100 + 1)){
         getSetByMakerAndArt()
+    } else {
+        page = 1
     }
 }
 
-function createSetButton(title, maker, objNumber){
+function createSetButton(title, maker, objNumber){ //cria os botões com as obras encontradas de acordo com o termo pesquisado
     var button = document.createElement('button')
+    button.classList.add('btn')
+    button.classList.add('btn-primary')
     button.classList.add('btn-maker')
     button.innerHTML = `${title} - ${maker}`
     btnMaker.appendChild(button)
@@ -127,55 +74,30 @@ function createSetButton(title, maker, objNumber){
     button.addEventListener('click', function(){getSetByAPI(title, objNumber)})
 }
 
-async function getSetByAPI(title, objNumber){
+async function getSetByAPI(title, objNumber){ //faz nova consulta a API retornando a obra selecionada
 
     const url = `https://www.rijksmuseum.nl/api/en/collection/${objNumber}/tiles?key=${key}`
 
     const res = await fetch(url)
    
     masterpieces = await res.json()
-    
-    console.log(masterpieces.levels[4])
-    showSet(title, masterpieces.levels[4])
+
+    console.log(masterpieces)
+
+    var level = masterpieces.levels.length - 2
+    var size = 'z' + level.toString()
+
+    for(let i = 0; i < masterpieces.levels.length; i++){
+        if(masterpieces.levels[i].name === size){
+            showSet(title, masterpieces.levels[i])
+        }
+    }     
 }
 
-// function getMaker(){ //faz a consulta pelo input, criando botões com o resultado
-//     btnMaker.innerHTML = ''
-
-//     var makers = [] //para verificar se existe mais de uma obra com o mesmo nome
-
-//     search = input.value //pega o valor da pesquisa
-
-//     const array = masterpieces.facets[0].facets //array com todos os pintores
-
-//     for(let i = 0; i < array.length; i++){
-
-//         var pintor = array[i].key.split(' ')
-
-//         for(let j = 0; j < pintor.length; j++){
-//             if (search.toLowerCase() === pintor[j].toLowerCase()){ //verifica se o nome pesquisado se refere a algum pintor
-//                 makers.push(array[i].key) //adiciona o pintor no array
-//             }
-//         }   
-//     }
-
-//     //cria um botão para cada e chama a função do pintor selecionado
-//     for(let i = 0; i < makers.length; i++){
-//         var button = document.createElement('button')
-//         button.classList.add('btn-maker')
-//         button.innerHTML = makers[i]
-//         btnMaker.appendChild(button)
-//     }
-//     var buttons = document.querySelectorAll('.btn-maker')
-
-//     buttons.forEach(button => {
-//         button.addEventListener('click', function(){getSets(button.firstChild.data)})
-//     })
-    
-//     input.value = '' //limpa o campo de pesquisa
-// }
-
-function showSet(title, set){ //faz nova pesquisa à API, agora com o termo específico do pintor
+function showSet(title, set){ //estiliza a imagem a ser mostrada na tela
+    content.innerHTML = ''
+    content.style.display = 'block'
+    btnMaker.style.display = 'none'
 
     var setWidth
     var setHeight
@@ -208,6 +130,21 @@ function showSet(title, set){ //faz nova pesquisa à API, agora com o termo espe
     img.setAttribute('height', setHeight)
 
     figureCaption.innerHTML = title
+
+    var buttonClose = document.createElement('button')
+
+    buttonClose.classList.add('btn')
+    buttonClose.classList.add('btn-danger')
+    buttonClose.classList.add('btn-close')
+    buttonClose.innerHTML = 'X'
+
+    buttonClose.addEventListener('click', () => {
+        content.style.display = 'none'
+        btnMaker.style.display = 'block'
+        btnMaker.style.display = 'flex'
+    })
+
+    figureCaption.appendChild(buttonClose)
 
     figure.appendChild(img)
     figure.appendChild(figureCaption)
